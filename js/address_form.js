bf758dc4c94cd4922ebb152f82123c0daa403a6e
varshini <dreamvarshini@gmail.com>
2021-10-19 19:47:06 +0530

    MODIFIED: validating contact name on user input

--- js/address_form.js
+++ js/address_form.js
@@ -1,5 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.name-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new AddressBook()).name = name.value;
+            textError.textContent = "";
+
+        }
+        catch (e) {
+            textError.textContent = e;
+        }
+    });
     
 });
 
