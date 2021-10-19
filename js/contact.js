d8c373f099c39cdf401480e11126c2a5e2a3214c
varshini <dreamvarshini@gmail.com>
2021-10-19 19:52:05 +0530

    ADDED: getter and setter for contact name

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+const NAME_PATTERN = RegExp('^([A-Z]{1}[a-z]{2,})( [A-Z]{1}[a-z]{2,})*$');
+const ADDRESS_PATTERN = RegExp("^[a-zA-Z0-9 ,'-,]*$");
+const PHONE_NUMBER_PATTERN = RegExp("^[+]?[91]{2}[ ]?[6-9][0-9]{9}$");
+const ZIP_PATTERN = RegExp("[0-9]{3}[ ]?[0-9]{3}$");
+
+class Contact
+{
+   
+    get name()
+    {
+        return this._name;
+    }
+    set name(name)
+    {
+        if (NAME_PATTERN.test(name)) 
+        {
+            this._name = name;
+        }
+        else {
+            throw "Name is Invalid";
+        }
+    }
+
+    
+
+}
