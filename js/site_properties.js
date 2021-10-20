5bf64f4b86e306b935d68a82d3efcad93f2470de
varshini <dreamvarshini@gmail.com>
2021-10-20 13:32:04 +0530

    REFACTORED: site property to include server url and local storage

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,6 +1,8 @@
 let site_properties=
 {
+    use_local_storage:"true",
     home_page:"../pages/home_page.html",
     address_form_page:"../pages/address_form.html",
+    server_url:"http://127.0.0.1:3000/AddressBook/"
     
 }
