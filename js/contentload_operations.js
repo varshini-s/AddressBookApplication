2ace01a7eafab2467050027aca59a2d81c1dcb1f
varshini <dreamvarshini@gmail.com>
2021-10-20 19:24:13 +0530

    REFACTORED: creating seperate functions for each functionality

--- js/contentload_operations.js
+++ js/contentload_operations.js
@@ -1,11 +1,96 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    const form=document.querySelector('.form');
-    form.addEventListener('input',function()
-    {
-        document.getElementById("addButton").disabled = false;
+    setAddButtonDisableProperty();
+    sortAndDisplayStateOptions();
+
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.name-error');
+    validateAndDisplayError(name, textError, checkName);
+
+    const phoneNumber = document.querySelector('#phoneNumber');
+    const phoneNumberError = document.querySelector('.phone-number-error');
+    validateAndDisplayError(phoneNumber, phoneNumberError, checkPhoneNumber);
+
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    validateAndDisplayError(address, addressError, checkAddress);
+
+    const zip = document.querySelector('#zip');
+    const zipError = document.querySelector('.zip-error');
+    validateAndDisplayError(zip, zipError, checkZip);
+
+    document.querySelector('.cancelLink').href = site_properties.home_page;
+    checkForUpdate();
+
+
+});
+
+const setAddButtonDisableProperty = () => {
+
+    checkAllInputFieldsAreFilled();
+    checkNoInvalidInput();
+};
+
+
+const checkAllInputFieldsAreFilled = () => {
+    const name = document.querySelector('#name');
+    const phoneNumber = document.querySelector('#phoneNumber');
+    const address = document.querySelector('#address');
+    const zip = document.querySelector('#zip');
+    const city = document.querySelector('#city');
+    const state = document.querySelector('#state');
+
+    const form = document.querySelector('.form');
+    form.addEventListener('input', function () {
+        if (name.value == "" || phoneNumber.value == "" || address.value == "" || zip.value == "" || state.selectedIndex == 0 || city.selectedIndex == 0) {
+            document.getElementById("addButton").disabled = true;
+        }
+        else {
+            document.getElementById("addButton").disabled = false;
+
+        }
+    });
+};
+
+const checkNoInvalidInput = () => {
+    const form = document.querySelector('.form');
+    form.addEventListener('input', () => {
+        const textError = document.querySelector('.name-error');
+        const phoneNumberError = document.querySelector('.phone-number-error');
+        const addressError = document.querySelector('.address-error');
+        const zipError = document.querySelector('.zip-error');
+
+        if (textError.textContent != "" || phoneNumberError.textContent != "" || addressError.textContent != "" || zipError.textContent != "") {
+            document.getElementById("addButton").disabled = true;
+        }
+        else {
+            document.getElementById("addButton").disabled = false;
+
+        }
+    });
+};
+
+const validateAndDisplayError = (inputField, errorField, validationFunction) => {
+
+    inputField.addEventListener('input', function () {
+        if (inputField.value.length == 0) {
+            errorField.textContent = "";
+            return;
+        }
+        try {
+            validationFunction(inputField.value);
+            errorField.textContent = "";
+
+        }
+        catch (e) {
+            errorField.textContent = e;
+        }
     });
 
+};
+
+
+const sortAndDisplayStateOptions = () => {
 
     const stateList = document.querySelector('#state').options;
 
@@ -19,81 +104,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     sortedStateList.sort();
 
     let stateOptions = "<option value=\"\" disabled selected>Select State</option>";
-    for (let state in sortedStateList) 
-    {
+    for (let state in sortedStateList) {
         stateOptions += "<option>" + sortedStateList[state] + "</option>";
     }
     document.getElementById("state").innerHTML = stateOptions;
 
-
-    const name = document.querySelector('#name');
-    const textError = document.querySelector('.name-error');
-    name.addEventListener('input', function () {
-        if (name.value.length == 0) {
-            textError.textContent = "";
-            return;
-        }
-        try {
-            (new Contact()).name = name.value;
-            textError.textContent = "";
-
-        }
-        catch (e) {
-            textError.textContent = e;
-        }
-    });
-
-    const phoneNumber = document.querySelector('#phoneNumber');
-    const phoneNumberError = document.querySelector('.phone-number-error');
-    phoneNumber.addEventListener('input', function () {
-        if (phoneNumber.value.length == 0) {
-            phoneNumberError.textContent = "";
-            return;
-        }
-        try {
-            (new Contact()).phoneNumber = phoneNumber.value;
-            phoneNumberError.textContent = "";
-
-        }
-        catch (e) {
-            phoneNumberError.textContent = e;
-        }
-    });
-
-    const address = document.querySelector('#address');
-    const addressError = document.querySelector('.address-error');
-    address.addEventListener('input', function () {
-        if (address.value.length == 0) {
-            addressError.textContent = "";
-            return;
-        }
-        try {
-            (new Contact()).address = address.value;
-            addressError.textContent = "";
-
-        }
-        catch (e) {
-            addressError.textContent = e;
-        }
-    });
-
-    const zip = document.querySelector('#zip');
-    const zipError = document.querySelector('.zip-error');
-    zip.addEventListener('input', function () {
-        if (zip.value.length == 0) {
-            zipError.textContent = "";
-            return;
-        }
-        try {
-            (new Contact()).zip = zip.value;
-            zipError.textContent = "";
-
-        }
-        catch (e) {
-            zipError.textContent = e;
-        }
-    });
-
-    checkForUpdate();
-
-});
+}
